4aebc98487be61f8b3bde9de77aac2c87aab102e
Douglas Brion <db1415@ic.ac.uk>
2019-04-22 09:20:43 +0100

    Adjusted X and Y Axis offsets to -31mm and -24mm to improve centering of object on build plate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -876,8 +876,8 @@
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -53
-#define Y_MIN_POS -20
+#define X_MIN_POS -31
+#define Y_MIN_POS -24
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
